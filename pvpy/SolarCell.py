Fixed the tilt conversion to radians. Began to add Fill Factor. Need to correct area and tilt factor effects.

pvpy/SolarCell.py
@@ -20,23 +20,27 @@ class SolarCell(object):
         """
         self.bandgap = bandgap
         self.bandgap_lambda = ev_to_nm(bandgap)
-        if not degrees:
-            self.tilt = tilt * 180/constants.pi
+        if degrees:
+            self.tilt = tilt * constants.pi / 180
+            print(self.tilt)
         else:
             self.tilt = tilt
         self.celltemp = celltemp
+        self.Vc = constants.k * self.celltemp / constants.e
         start_w = 280
         stop_w = int(np.around(self.bandgap_lambda))
         if back_reflector:
             self.area = area
+            self.solid_emission_angle = PowerSpectrum.solid_angle(180, 180)
         if not back_reflector:
-            self.area = area * 2
-        self.solid_emission_angle = PowerSpectrum.solid_angle(180, 180)
+            self.area = area * 1
+            self.solid_emission_angle = PowerSpectrum.solid_angle(180, 360)
         # give the cell an initial perfect absorptivity for SQ type analysis
         wavelengths = np.arange(start_w, stop_w + 1, dtype=int)
         self.absorptivity = np.vstack((wavelengths, np.ones(wavelengths.shape))).T
         # give the cell a black body spectrum
-        self.cell_bb_spectrum = PhotocurrentSpectrum(start_w=start_w, stop_w=20000, bbtemp=self.celltemp,
+        # TODO: Figure out why I can't put in the bandgap wavelength to stop_w without dividing by zero
+        self.cell_bb_spectrum = PhotocurrentSpectrum(start_w=start_w, stop_w=5000, bbtemp=self.celltemp,
                                                      spectra="BlackBody",
                                                      solidangle=self.solid_emission_angle)
         self.cell_bb_spectrum.weight_spectrum(self.absorptivity)
@@ -55,6 +59,8 @@ class SolarCell(object):
         self.illuminationspectrum = self.illuminationspectrumAmbient
         self.j_nonrad = 0  # Units of amp/m^2
         self.ideality = 1
+        self.Voc = self.get_Voc()
+        self.Isc = self.get_current()
 
     def set_absorptivity(self, alpha):
         self.absorptivity = alpha
@@ -66,6 +72,7 @@ class SolarCell(object):
         # weight the illumination spectrum by the cells probability of absorbing light, and the probability that
         # it will generate an electron-hole pair
         weight = self.absorptivity.copy()
+        # TODO: Tilt factor does not give expected results. The Isc changes for different LED_eff with nonzero tilt, which doesn't make sense to me
         weight[:, 1] = self.absorptivity[:, 1] * self.IQE[:, 1] * self.area * np.cos(self.tilt)
         photonspectrum.weight_spectrum(weight)
         self.generation = photonspectrum.integrate()
@@ -76,7 +83,8 @@ class SolarCell(object):
         Vc = constants.k * self.celltemp / constants.e
         n = self.ideality
         vweight = luminescence_spectrum.get_spectrum()
-        vweight[:, 1] *= np.exp(v / (Vc * n))
+        # TODO: Determine need for chemical potential of excited carriers correction to exp(v/vc) vs. exp((v-u)/vc)
+        vweight[:, 1] *= np.exp((v - self.Voc) / (Vc * n)) * self.absorptivity[:, 1] * self.IQE[:, 1] * self.area
         luminescence_spectrum.weight_spectrum(vweight)
         return luminescence_spectrum
 
@@ -88,6 +96,8 @@ class SolarCell(object):
     def set_illumination(self, illuminationspectrum):
         self.illuminationspectrum = illuminationspectrum
         self.__set_generation(self.illuminationspectrum)
+        self.Voc = self.get_Voc()
+        self.Isc = self.get_current()
         return
 
     def get_current(self, v=None):
@@ -98,6 +108,7 @@ class SolarCell(object):
         # aka the non-radiative recombination at v = 0
         Fc0 = self.cell_bb_spectrum  # Fc0
         Fc0 = Fc0.integrate()
+        # Below is only valid for ideal rectifier/diode equation
         R0 = Fc0 * (1 - self.LED_eff) / self.LED_eff  # R(0)
         # Next, get the blackbody radiation from the cell at v voltage
         FcV = self.luminescence_spectrum(self.voltage)
@@ -107,10 +118,11 @@ class SolarCell(object):
         n = self.ideality
         if v is None:
             v = self.voltage
-        assert isinstance(v, (int, float)), "Voltage %g is not an int or float." % v
+        assert isinstance(v, (int, float, list, np.ndarray)), "Voltage %g is not an int or float." % v
         # TODO: Implement Double Diode Model Here?
         Rv = R0 * np.exp(v / (Vc * n))  # R(V)
-        #print("Fs: %f, FcV: %f, R(0): %f, R(V): %f" % (Fs, FcV, R0, Rv))  # For Debugging
+        # print("Fs: %g, FcV: %g, R(0): %g, R(V): %g" % (Fs, FcV, R0, Rv))  # For Debugging
+        # TODO: Determine need for chemical potential of excited carriers correction to exp(v/vc) vs. exp((v-u)/vc)
         current = Fs - Fc0 + (Fc0 / self.LED_eff) * (1 - np.exp(v / (Vc * n)))
         return current
 
@@ -118,6 +130,13 @@ class SolarCell(object):
         self.__set_generation(self.illuminationspectrum)
         external_generation = self.generation
         cell_recombination0 = self.cell_bb_spectrum.integrate()
-        Vc = constants.k * self.celltemp / constants.e
+        Vc = self.Vc
         Voc = Vc * np.log((self.LED_eff * external_generation / cell_recombination0) - self.LED_eff + 1)
+        self.Voc = Voc
         return Voc
+
+    def get_fill_factor(self):
+        # Following section 5 of Shockley Queisser 1961
+        zop = self.Voc / self.Vc
+        # vmax =
+        pass