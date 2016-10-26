Added fill factor and effciency. Need to fix set_illumination/weighting still. Code not usable right now.

pvpy/PowerSpectrum.py
@@ -10,7 +10,7 @@ SOLAR_SOLID_ANGLE = 2 * constants.pi * (1 - np.cos(.009303575/2))  # ~6.798e-05 
 
 class PowerSpectrum(object):
     def __init__(self, start_w=280.0, stop_w=4000.0, spectra="AM1.5G", bbtemp=5800, mediumrefindex=1,
-                 solidangle=SOLAR_SOLID_ANGLE, userspectrum=None):
+                 solidangle=SOLAR_SOLID_ANGLE, userspectrum=None, v=0):
         """
         Initilizer for PowerSpectrum class. Builds custom spectrum if variables are passed when creating instance.
         :param start_w: shortest wavelength in nanometers
@@ -26,8 +26,12 @@ class PowerSpectrum(object):
         a source encompassing the whole sphere it can see is 4pi.
         :return:
         """
-        self.start_w = int(np.around(start_w))
-        self.stop_w = int(np.around(stop_w)) + 1
+        self.bbtemp = bbtemp
+        self.mediumrefindex = mediumrefindex
+        self.solidangle = solidangle
+        self.v = v
+        self.start_w = start_w
+        self.stop_w = stop_w
         # the first column should be the wavelength in nanometers, the second is the tilt power density/nm in
         # W/(m**2 nm) = J s^-1 m^-2 nm^-1 = C V m^-2 nm^-1
         if userspectrum is not None:
@@ -43,10 +47,12 @@ class PowerSpectrum(object):
         if spectra_ind in range(4):
             self.spectrum = np.genfromtxt(path.join(path.dirname(__file__), './ASTMG173.csv'), delimiter=",",
                                           skip_header=2)[:, [0, spectra_ind]]
-            # build custom spectrum if necessary
             if start_w != 280.0 or stop_w != 4000.0:
                 self.spectrum = self.sub_spectrum(start_w, stop_w)
+                print(self.spectrum[-1])
         elif spectra == "BlackBody":
+            self.start_w = start_w
+            self.stop_w = stop_w + 1
             self.spectrum = self.blackbody_spectrum(mediumrefindex, solidangle, bbtemp)
         elif spectra == "User":
             assert isinstance(userspectrum, np.ndarray), "Weight spectrum is not a 2D numpy array."
@@ -54,7 +60,7 @@ class PowerSpectrum(object):
         # create the PowerSpectrum interpolator
         self.interp = interpolate.interp1d(self.spectrum[:, 0], self.spectrum[:, 1])
 
-    def blackbody_spectrum(self, mediumrefindex=1, solidangle=4*constants.pi, bbtemp=5400):
+    def blackbody_spectrum(self, mediumrefindex=1, solidangle=4*constants.pi, bbtemp=5400, v=0):
         """
         Creates a blackbody distribution of power flux for a given solid angle. Default is entire sphere for the sun.
         :param mediumrefindex: refractive index of medium surrounding blackbody
@@ -71,7 +77,7 @@ class PowerSpectrum(object):
         wavelengths = np.arange(self.start_w, self.stop_w, dtype=float) * 1e-9
         # 2n^2hc^2/lambda^5*(exp(-hc/k lambda T)-1) gives units of W sr^-1 m^-3
         numerator = 2 * (self.mediumrefindex ** 2) * constants.h * constants.c ** 2
-        exponential = np.exp(constants.h * constants.c / (constants.k * wavelengths * bbtemp))
+        exponential = np.exp((constants.h * constants.c - v) / (constants.k * wavelengths * bbtemp))
         spectrum[:, 1] = numerator / ((wavelengths ** 5) * (exponential - 1))
         # Use provided solid angle to and 1m=1-9 get Power Flux per nanometer
         spectrum[:, 1] *= 1e-9 * self.solidangle
@@ -116,9 +122,9 @@ class PowerSpectrum(object):
             if not lowerb <= w <= upperb:
                 print("Wavelength %0.2f nm out of spectra bounds" % w)
                 if w < lowerb:
-                    raise IndexError("Please use the lower bound of %0.2f nm." % lowerb)
+                    raise IndexError("Wavelength %0.2f too short. Please use the lower bound of %0.2f nm." % (w, lowerb))
                 elif w > upperb:
-                    raise IndexError("Please use the upper bound of %0.2f nm." % upperb)
+                    raise IndexError("Wavelength %0.2f too large. Please use the upper bound of %0.2f nm." % (w, upperb))
             else:
                 pass
         return
@@ -151,6 +157,10 @@ class PowerSpectrum(object):
         power_f = integrate.trapz(spectrum[:, 1], spectrum[:, 0])
         return power_f  # Units Watts/meters^2
 
+    def get_incident_power(self):
+        # Does not include sub-bandgap power
+        return self.integrate()
+
     def get_spectrum(self):
         """
         Returns a copy of the spectrum.
@@ -178,7 +188,7 @@ class PowerSpectrum(object):
         assert spec_in.shape[1] == 2, "Weight spectrum is not a 2D numpy array."
         #TODO: Catch errors for when there aren't enough points for default kind, i.e. uncommon case of <4 points in weight
         spec_fun = interpolate.interp1d(spec_in[:, 0], spec_in[:, 1], kind=kind)
-        if spec_in[0, 0] != self.start_w or spec_in[-1, 0] != self.stop_w - 1:
+        if spec_in[0, 0] >= self.start_w or spec_in[-1, 0] <= self.stop_w:
             self.spectrum = self.sub_spectrum(spec_in[0, 0], spec_in[-1, 0])
         spec_wt = self.spectrum
         spec_wt[:, 1] = spec_fun(spec_wt[:, 0]) * spec_wt[:, 1]
@@ -187,10 +197,21 @@ class PowerSpectrum(object):
     def copy(self):
         return copy(self)
 
+    def to_PhotonSpectrum(self):
+        self.spectrum[:, 1] *= (self.spectrum[:, 0] * 1e-9 / (constants.c * constants.h))
+        self.interp = interpolate.interp1d(self.spectrum[:, 0], self.spectrum[:, 1])
+        self.__class__ = PhotonSpectrum
+        return
+
+    def to_PhotoCurrentSpectrum(self):
+        self.to_PhotonSpectrum()
+        self.to_PhotoCurrentSpectrum()
+        return
+
 
 class PhotonSpectrum(PowerSpectrum):
-    def __init__(self, start_w= 280.0, stop_w= 4000.0, spectra= "AM1.5G", bbtemp = 5800, mediumrefindex=1,
-                 solidangle=SOLAR_SOLID_ANGLE, userspectrum=None):
+    def __init__(self, start_w=280.0, stop_w=4000.0, spectra="AM1.5G", bbtemp=5800, mediumrefindex=1,
+                 solidangle=SOLAR_SOLID_ANGLE, userspectrum=None, v=0):
         """
         Initilizer for PowerSpectrum class. Builds custom spectrum if variables are passed when creating instance.
         :param start_w: shortest wavelength in nanometers
@@ -206,14 +227,38 @@ class PhotonSpectrum(PowerSpectrum):
         a source encompassing the whole sphere it can see is 4pi.
         :return:
         """
-        super(PhotonSpectrum, self).__init__(start_w, stop_w, spectra, bbtemp, mediumrefindex, solidangle)
-        self.spectrum[:, 1] = self.spectrum[:, 1] * (self.spectrum[:, 0] * 1e-9 / (constants.c * constants.h))
+        super(PhotonSpectrum, self).__init__(start_w=start_w,
+                                             stop_w=stop_w,
+                                             spectra=spectra,
+                                             bbtemp=bbtemp,
+                                             mediumrefindex=mediumrefindex,
+                                             solidangle=solidangle,
+                                             userspectrum=userspectrum,
+                                             v=v)
+        self.spectrum[:, 1] *= (self.spectrum[:, 0] * 1e-9 / (constants.c * constants.h))
         self.interp = interpolate.interp1d(self.spectrum[:, 0], self.spectrum[:, 1])
+
+    def to_PowerSpectrum(self):
+        self.spectrum[:, 1] /= (self.spectrum[:, 0] * 1e-9 / (constants.c * constants.h))
+        self.interp = interpolate.interp1d(self.spectrum[:, 0], self.spectrum[:, 1])
+        self.__class__ = PowerSpectrum
+        return
+
+    def to_PhotoCurrentSpectrum(self):
+        self.spectrum[:, 1] *= constants.e
+        self.interp = interpolate.interp1d(self.spectrum[:, 0], self.spectrum[:, 1])
+        self.__class__ = PhotocurrentSpectrum
+        return
+
+    def get_incident_power(self):
+        spectrum_copy = self.copy()
+        spectrum_copy.to_PowerSpectrum()
+        return spectrum_copy.get_incident_power()
 
 
 class PhotocurrentSpectrum(PhotonSpectrum):
-    def __init__(self, start_w= 280.0, stop_w= 4000.0, spectra= "AM1.5G", bbtemp = 5800, mediumrefindex=1,
-                 solidangle=SOLAR_SOLID_ANGLE, userspectrum=None):
+    def __init__(self, start_w=280.0, stop_w=4000.0, spectra="AM1.5G", bbtemp= 5800, mediumrefindex=1,
+                 solidangle=SOLAR_SOLID_ANGLE, userspectrum=None, v=0):
         """
         Initilizer for PowerSpectrum class. Builds custom spectrum if variables are passed when creating instance.
         :param start_w: shortest wavelength in nanometers
@@ -229,6 +274,24 @@ class PhotocurrentSpectrum(PhotonSpectrum):
         a source encompassing the whole sphere it can see is 4pi.
         :return:
         """
-        super(PhotocurrentSpectrum, self).__init__(start_w, stop_w, spectra, bbtemp, mediumrefindex, solidangle)
+        super(PhotocurrentSpectrum, self).__init__(start_w=start_w,
+                                                   stop_w=stop_w,
+                                                   spectra=spectra,
+                                                   bbtemp=bbtemp,
+                                                   mediumrefindex=mediumrefindex,
+                                                   solidangle=solidangle,
+                                                   userspectrum=userspectrum,
+                                                   v=v)
         self.spectrum[:, 1] *= constants.e
         self.interp = interpolate.interp1d(self.spectrum[:, 0], self.spectrum[:, 1])
+
+    def to_PhotonSpectrum(self):
+        self.spectrum[:, 1] /= constants.e
+        self.interp = interpolate.interp1d(self.spectrum[:, 0], self.spectrum[:, 1])
+        self.__class__ = PhotonSpectrum
+        return
+
+    def to_PowerSpectrum(self):
+        self.to_PhotonSpectrum()
+        self.to_PowerSpectrum()
+        return

pvpy/SolarCell.py
@@ -1,6 +1,8 @@
 import numpy as np
-from scipy import interpolate, integrate, constants
-from PowerSpectrum import PowerSpectrum, PhotocurrentSpectrum, PhotonSpectrum
+from scipy import constants, interpolate
+from scipy.optimize import fmin
+from PowerSpectrum import PowerSpectrum, PhotocurrentSpectrum
+import warnings
 
 
 def ev_to_nm(ev):
@@ -8,59 +10,67 @@ def ev_to_nm(ev):
 
 
 class SolarCell(object):
-    def __init__(self, bandgap=1.1, area=1, tilt=0, degrees=True, back_reflector=True, celltemp=300):
+    def __init__(self, bandgap=1.1, tilt=0, degrees=True, back_reflector=True, celltemp=300):
         """
 
         :param bandgap:
-        :param area:
         :param tilt:
         :param degrees:
-        :param BBtemp:
+        :param celltemp:
         :param back_reflector:
         """
+        if bandgap < 0.2:
+            warnings.warn("These results will be inaccurate for bandgaps smaller than 0.2eV.")
         self.bandgap = bandgap
         self.bandgap_lambda = ev_to_nm(bandgap)
         if degrees:
             self.tilt = tilt * constants.pi / 180
-            print(self.tilt)
         else:
             self.tilt = tilt
         self.celltemp = celltemp
-        self.Vc = constants.k * self.celltemp / constants.e
+        self.ideality = 1
+        self.Vc = constants.k * self.celltemp * self.ideality / constants.e
         start_w = 280
         stop_w = int(np.around(self.bandgap_lambda))
         if back_reflector:
-            self.area = area
             self.solid_emission_angle = PowerSpectrum.solid_angle(180, 180)
         if not back_reflector:
-            self.area = area * 1
             self.solid_emission_angle = PowerSpectrum.solid_angle(180, 360)
+        # Default illumination spectrum is ambient blackbody
+        self.illuminationspectrumAmbient = PhotocurrentSpectrum(start_w=start_w, stop_w=stop_w,
+                                                                spectra="BlackBody",
+                                                                bbtemp=self.celltemp,
+                                                                solidangle=self.solid_emission_angle)
         # give the cell an initial perfect absorptivity for SQ type analysis
-        wavelengths = np.arange(start_w, stop_w + 1, dtype=int)
-        self.absorptivity = np.vstack((wavelengths, np.ones(wavelengths.shape))).T
+        self.absorptivity = self.illuminationspectrumAmbient.get_spectrum()
+        print(self.absorptivity[-1])
+        self.absorptivity[:, 1] = np.ones(self.absorptivity[:, 1].shape)
+        # This is an artifact of the ASTM spectrums having nonuniform bins, we have to set
+        # the cell band gap to its nearest wavelength
+        self.bandgap_lambda = self.absorptivity[-1, 0]
         # give the cell a black body spectrum
         # TODO: Figure out why I can't put in the bandgap wavelength to stop_w without dividing by zero
         self.cell_bb_spectrum = PhotocurrentSpectrum(start_w=start_w, stop_w=5000, bbtemp=self.celltemp,
                                                      spectra="BlackBody",
                                                      solidangle=self.solid_emission_angle)
         self.cell_bb_spectrum.weight_spectrum(self.absorptivity)
-        # Default illumination spectrum is ambient blackbody
-        self.illuminationspectrumAmbient = PhotocurrentSpectrum(start_w=start_w, stop_w=stop_w,
-                                                                spectra="BlackBody",
-                                                                bbtemp=self.celltemp,
-                                                                solidangle=self.solid_emission_angle)
         self.voltage = 0
         self.luminescencespectrum = self.cell_bb_spectrum.copy()
         # Initilize the IQE as perfect by copying the unity absorptivity (i.e. normalized absorption)
         self.IQE = self.absorptivity.copy()
         self.LED_eff = 1
         self.generation = 0
-
         self.illuminationspectrum = self.illuminationspectrumAmbient
+        self.incident_power = self.illuminationspectrum.get_incident_power()
         self.j_nonrad = 0  # Units of amp/m^2
-        self.ideality = 1
         self.Voc = self.get_Voc()
         self.Isc = self.get_current()
+        # Initilize with some empirical guesses
+        # use initial empirical guess of Martin Green
+        self.ff = (self.Voc/self.Vc - np.log(self.Voc/self.Vc + .72)) / (self.Voc/self.Vc + 1)
+        self.Vmpp = self.ff * self.Isc
+        self.Impp = self.ff * self.Voc
+        self.maxpower = self.Vmpp * self.Impp
 
     def set_absorptivity(self, alpha):
         self.absorptivity = alpha
@@ -68,25 +78,32 @@ class SolarCell(object):
         self.luminescencespectrum = self.cell_bb_spectrum.copy()
         return
 
-    def __set_generation(self, photonspectrum):
+    def __set_generation(self, photocurrentspec):
         # weight the illumination spectrum by the cells probability of absorbing light, and the probability that
         # it will generate an electron-hole pair
         weight = self.absorptivity.copy()
-        # TODO: Tilt factor does not give expected results. The Isc changes for different LED_eff with nonzero tilt, which doesn't make sense to me
-        weight[:, 1] = self.absorptivity[:, 1] * self.IQE[:, 1] * self.area * np.cos(self.tilt)
-        photonspectrum.weight_spectrum(weight)
-        self.generation = photonspectrum.integrate()
+        weight[:, 1] = self.absorptivity[:, 1] * self.IQE[:, 1] * np.cos(self.tilt)
+        print(weight[-1])
+        weightfun = interpolate.interp1d(weight[:, 0], weight[:, 1])
+        print(photocurrentspec.get_spectrum())
+        weight = photocurrentspec.sub_spectrum(weight[0, 0], weight[-1, 0])
+        weight[:, 1] = weightfun(weight[:, 0])
+        photocurrentspec.weight_spectrum(weight)
+        self.generation = photocurrentspec.integrate()
         return self.generation
 
     def luminescence_spectrum(self, v=0):
-        luminescence_spectrum = self.cell_bb_spectrum.copy()
-        Vc = constants.k * self.celltemp / constants.e
-        n = self.ideality
-        vweight = luminescence_spectrum.get_spectrum()
-        # TODO: Determine need for chemical potential of excited carriers correction to exp(v/vc) vs. exp((v-u)/vc)
-        vweight[:, 1] *= np.exp((v - self.Voc) / (Vc * n)) * self.absorptivity[:, 1] * self.IQE[:, 1] * self.area
-        luminescence_spectrum.weight_spectrum(vweight)
+        # TODO: Determine if the chemical potential dependent blackbody spectrum will work for non-ideal diodes
+        luminescence_spectrum = PhotocurrentSpectrum(start_w=280, stop_w=self.bandgap_lambda,
+                                                     bbtemp=self.celltemp,
+                                                     spectra="BlackBody",
+                                                     solidangle=self.solid_emission_angle,
+                                                     v=v)
+        luminescence_spectrum.weight_spectrum(self.absorptivity)
         return luminescence_spectrum
+
+    def get_saturation_current(self):
+        return self.cell_bb_spectrum.integrate()
 
     def set_voltage(self, v=0):
         self.luminescencespectrum = self.luminescence_spectrum(v)
@@ -96,6 +113,10 @@ class SolarCell(object):
     def set_illumination(self, illuminationspectrum):
         self.illuminationspectrum = illuminationspectrum
         self.__set_generation(self.illuminationspectrum)
+        # TODO: Figure out when to set incident power. If user puts in a spectrum that is only defined to the bandgap
+        # then the "incident power" is lower than it should be. This is avoided by just using the default start_w stop_w
+        # for incident spectrums, but perhaps should require the user explicitly input?
+        # self.incident_power = illuminationspectrum.get_incident_power()
         self.Voc = self.get_Voc()
         self.Isc = self.get_current()
         return
@@ -120,9 +141,8 @@ class SolarCell(object):
             v = self.voltage
         assert isinstance(v, (int, float, list, np.ndarray)), "Voltage %g is not an int or float." % v
         # TODO: Implement Double Diode Model Here?
-        Rv = R0 * np.exp(v / (Vc * n))  # R(V)
+        # Rv = R0 * np.exp(v / (Vc * n))  # R(V)
         # print("Fs: %g, FcV: %g, R(0): %g, R(V): %g" % (Fs, FcV, R0, Rv))  # For Debugging
-        # TODO: Determine need for chemical potential of excited carriers correction to exp(v/vc) vs. exp((v-u)/vc)
         current = Fs - Fc0 + (Fc0 / self.LED_eff) * (1 - np.exp(v / (Vc * n)))
         return current
 
@@ -135,8 +155,19 @@ class SolarCell(object):
         self.Voc = Voc
         return Voc
 
+    def get_max_power(self):
+        normvoc = self.Voc / self.Vc
+        fffunc = lambda x: -1 * x * self.get_current(v=x) / (self.Voc * self.Isc)
+        self.Vmpp = fmin(fffunc, self.Voc * self.ff, disp=False)[0]
+        self.Impp = self.get_current(v=self.Vmpp)
+        return self.Vmpp * self.Impp
+
     def get_fill_factor(self):
-        # Following section 5 of Shockley Queisser 1961
-        zop = self.Voc / self.Vc
-        # vmax =
-        pass
+        self.maxpower = self.get_max_power()
+        self.ff = self.maxpower / (self.Voc * self.Isc)
+        return self.ff
+
+    def get_effciency(self):
+        self.get_fill_factor()
+        self.effciency = self.ff * self.Isc * self.Voc / self.incident_power
+        return self.effciency